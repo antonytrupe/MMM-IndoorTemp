d765d9ec31ec75a0022e1e3254d54c968355c164
antonytrupe <antony.trupe@gmail.com>
2020-04-14 20:10:45 -0400

    update

--- MMM-HouseStatus.js
+++ MMM-HouseStatus.js
@@ -1,6 +1,7 @@
 ﻿Module.register("MMM-HouseStatus", {
     // Default module config.
     defaults: {
+      updateInterval: 10*1000,//seconds * milliseconds
     },
 
     getStyles: function() {
@@ -10,44 +11,53 @@
     },
     getScripts:function(){return ['https://code.jquery.com/jquery.min.js'];},
 
+    // Define start sequence.
+    start: function() {
+      var self=this;
+      Log.info("Starting module: " + this.name);
+      self.sendSocketNotification('HOUSE_STATS');
+      setInterval(function() {
+        self.sendSocketNotification('HOUSE_STATS');
+        }, this.config.updateInterval);
+    },
+    
+    
+    
     notificationReceived: function(notification, payload, sender) {
         if(notification!='CLOCK_SECOND'){
-          Log.info('notificationReceived:'+notification);
+          //Log.info('notificationReceived:'+notification);
         }
         switch(notification) {
           case "DOM_OBJECTS_CREATED":
             //TODO start polling?
-            this.sendSocketNotification('GET_HOUSE_STATS');
+            //this.sendSocketNotification('HOUSE_STATS');
         }
       },
       
       socketNotificationReceived: function(notification,payload){
-        Log.info('socketNotificationReceived:'+notification+':'+payload);
-          if(notification==="HOUSE_STATS"){
-            //TODO update DOM
-            // [{'name':'living room','attributes':{'temp':20.2,'rh':.45}}]
-            payload.forEach((location) => {
-              var cssName='.'+location.name.split(' ').join('.');
-              console.log(cssName);
-              //TODO loop over the attributes
-              location.attributes.forEach((attribute)=>{
-                console.log(attribute);
-                $(cssName+' .' +attribute.name).html(attribute.value);
-              });
-            });
-            //$('.living.room .temp').html(payload);
-          }
+        //Log.info('socketNotificationReceived:'+notification+':'+payload);
+        if(notification==="HOUSE_STATS"){
+          //TODO update DOM
+          // [{'name':'living room','attributes':{'tempF':20.2,'rh':.45}}]
+          payload.forEach((row) => {
+            var cssName='.'+row.location.split(' ').join('.');
+            //console.log(cssName);
+            //TODO loop over the attributes
+            $(cssName+' .' +row.attribute).html(row.value);
+          });
+          //$('.living.room .temp').html(payload);
+        }
       },
 
     // Override dom generator.
     getDom: function() {
-      Log.info('getDom');
+      //Log.info('getDom');
         var wrapper = document.createElement("div");
 
         wrapper.innerHTML = '<div class="normal medium" >' +
             '<div class="living room">' +
-            '<span class=room_name>Living Room</span>'+
-            '<span class="temp"></span>' +
+            '<span class=room_name>Living Room:</span>'+
+            '<span class="tempC"></span>°' +
             '<span class="rh"></span>' +
             '</div>' +
             '</div>';
